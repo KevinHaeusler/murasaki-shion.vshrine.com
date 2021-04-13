a6a623f751bf563c7ec15356b7971d5f0e5e07bb
Kevin Haeusler <kevin.haeusler@khdev.ch>
2021-04-13 10:36:20 +0200

    colors

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,35 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">murasaki-shion.vshrine.com</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
+  <div class="w-screen bg-accent pb-5">
+    <div class="mx-auto max-w-screen-lg">
+      <h1 class="text-primary text-center font-bold text-5xl">
+        <span>Murasaki</span> <span class="text-primary-dark">Shion</span>
+        <span class="text-secondary">Virtual</span>
+        <span class="text-white">Shrine</span>
+      </h1>
+      <p class="text-black text-center mt-5">
+        This website is a work in progress, feel free to checkout the
+        <a href="https://github.com/KevinHaeusler/minato-aqua.vshrine.com"
+          >Github</a
         >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
+      </p>
+      <div class="lg:flex mt-5 lg:place-content-center">
+        <div class="w-40 h-40 mr-5 bg-primary text-white text-center shadow">
+          Primary
+        </div>
+        <div
+          class="w-40 h-40 mr-5 bg-primary-dark text-white text-center shadow"
         >
-          GitHub
-        </a>
+          Primary Dark
+        </div>
+        <div class="w-40 h-40 mr-5 bg-secondary text-white text-center shadow">
+          Secondary
+        </div>
+        <div class="w-40 h-40 mr-5 bg-accent text-black text-center shadow">
+          Accent
+        </div>
+        <div class="w-40 h-40 mr-5 bg-white text-black text-center shadow">
+          White
+        </div>
       </div>
     </div>
   </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#8656A6',
+          dark: '#343343',
+        },
+        secondary: '#827EF2',
+        accent: {
+          DEFAULT: '#6FBF36',
+          light: '#C7D943',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
